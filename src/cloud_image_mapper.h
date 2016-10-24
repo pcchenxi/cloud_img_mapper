c29fceb35ff1f512bc3ff3649e3b429a0814e6f5
xi <xi8@kth.se>
2016-10-24 11:48:42 +0200

    link to UBO slam

--- src/cloud_image_mapper.h
+++ src/cloud_image_mapper.h
@@ -17,16 +17,18 @@
 #include <pcl_conversions/pcl_conversions.h>
 
 using namespace std;
+using namespace cv;
 
 class Cloud_Image_Mapper
 {
   tf::TransformListener *tf_listener_;
   image_geometry::PinholeCameraModel cam_model_;
   CvFont font_;
-  
   //sensor_msgs::PointCloud2 transform_cloud(sensor_msgs::PointCloud2ConstPtr cloud_in, string frame_target);
 
 public:
+  Mat img_ori, image_label;
+  Mat img_label_grey_, img_label_color_;
   Cloud_Image_Mapper(tf::TransformListener *tf_listener)
   {
     tf_listener_ = tf_listener;
@@ -83,6 +85,12 @@ public:
       input_bridge = cv_bridge::toCvCopy(image_msg, sensor_msgs::image_encodings::BGR8);
       image = input_bridge->image;
       image_display = image.clone();
+
+      ostringstream file_name;
+      file_name << "/home/xi/workspace/catkin_rcv/image/" << ros::Time::now() << "_img.jpg";
+    
+      ROS_ASSERT( cv::imwrite( file_name.str(), image ) );  
+      // ROS_ASSERT( cv::imwrite( "img.jpg",  input_bridge->image ) );  
     }
     catch (cv_bridge::Exception& ex){
       ROS_ERROR("[draw_frames] Failed to convert image");
@@ -90,7 +98,8 @@ public:
     }
     
     cv::Mat img_mindist(image.rows, image.cols, CV_8UC1, cv::Scalar(255));
-    
+    cv::Mat img_label(image.rows, image.cols, CV_8UC1, cv::Scalar(0));
+
    // read camera information
     cam_model_.fromCameraInfo(info_msg);
 
@@ -101,6 +110,9 @@ public:
     for(int i = 0; i < pcl_cloud.points.size(); i++)
     {
       pcl::PointXYZRGB point = pcl_cloud.points[i];
+      if(point.x == 0 && point.y == 0 && point.z == 0)
+        continue;
+
       if(point.z < 0)
         continue;
       
@@ -153,7 +165,7 @@ public:
     for(int i = 0; i < pcl_cloud.points.size(); i++)
     {
       pcl::PointXYZRGB point = pcl_cloud.points[i];
-      if(point.z < 0)
+      if(point.z < 0 || abs(point.x) > 6)
         continue;
       
       cv::Point3d pt_cv(point.x, point.y, point.z);
@@ -162,7 +174,7 @@ public:
       uv = cam_model_.project3dToPixel(pt_cv);
 
       
-      static const int RADIUS = 5;
+      static const int RADIUS = 7;
       //cout << uv << endl;
       
       if(uv.x >= 0 && uv.x < image.cols && uv.y >= 0 && uv.y < image.rows)
@@ -171,29 +183,64 @@ public:
         uchar point_g = pcl_cloud.points[i].g;
         uchar point_b = pcl_cloud.points[i].b;
 
-        uchar min_z = img_mindist.at<uchar>(uv.y, uv.x);
-        if(abs(point.z/0.02 - min_z) < 10 )
-	      // if(point.z/0.02 < min_z )
-        {
-          cv::Vec3b intensity = image_display.at<cv::Vec3b>(uv.y, uv.x);
+        // uchar min_z = img_mindist.at<uchar>(uv.y, uv.x);
+        // if(abs(point.z/0.02 - min_z) < 10 )
+	      // // if(point.z/0.02 < min_z )
+        // {
+        //   cv::Vec3b intensity = image_display.at<cv::Vec3b>(uv.y, uv.x);
 
-          uchar r = intensity.val[2];
-          uchar g = intensity.val[1];
-          uchar b = intensity.val[0];
+        //   uchar r = intensity.val[2];
+        //   uchar g = intensity.val[1];
+        //   uchar b = intensity.val[0];
           
-          velodyne_cloud.points[i].r = r;
-          velodyne_cloud.points[i].g = g;
-          velodyne_cloud.points[i].b = b;
+        //   velodyne_cloud.points[i].r = r;
+        //   velodyne_cloud.points[i].g = g;
+        //   velodyne_cloud.points[i].b = b;
           
-          // cloud_vision_label[i] = (r+g+b)/3;
-          //cv::line(image, p1, p2, CV_RGB(255,0,0), 1);
-        }
+        //   // cloud_vision_label[i] = (r+g+b)/3;
+        //   //cv::line(image, p1, p2, CV_RGB(255,0,0), 1);
+        // }
         cv::circle(image, uv, RADIUS, CV_RGB(point_r,point_g,point_b), -1);    
+
+        int color_grey = 0;
+        if(point_r == 50)
+          color_grey = 50;
+        else if(point_r == 254)
+          color_grey = 150;
+        else if(point_r == 255)
+          color_grey = 255;  
+
+
+        if(point.z > 5)
+          continue;
+
+        float scaled_depth = 255/5 * point.z;
+        uchar depth = 255 - scaled_depth;
+
+        cv::circle(img_label, uv, 5, CV_RGB(depth, depth, depth), -1);    
+
+        // cv::circle(img_label, uv, 5, CV_RGB(color_grey,color_grey,color_grey), -1); 
       }
     }
     
-    cv::imshow("min", image);
-    cv::waitKey(5);
+    // Mat element = getStructuringElement(MORPH_RECT, Size(18,18));
+    // morphologyEx(img_label, img_label, MORPH_DILATE, element);
+
+    Mat output;
+
+    float alpht = 0.5;
+    addWeighted(image, alpht, image_display, 1-alpht, 0, output);
+
+    img_label_grey_  = img_label;
+    img_label_color_ = output;
+
+   ostringstream file_name;
+   file_name << "/home/xi/workspace/catkin_rcv/image/" << ros::Time::now() << "_label.png";
+  //  ROS_ASSERT( cv::imwrite( file_name.str(), img_label ) );  
+
+//    cv::imshow("img", img_label_color_);
+   cv::imshow("label", img_label_grey_);
+   cv::waitKey(5);
 
     return velodyne_cloud;
   }
